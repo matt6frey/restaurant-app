ab4e6e5cde3ca3c870d6f7d621bb04cd61be543c
Matt Frey <matt6frey@gmail.com>
2018-06-11 19:52:44 +0000

    More changes to knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,8 @@ module.exports = {
   development: {
     client: 'postgres',
     connection: {
-      host     : process.env.DATABASE_URL || process.env.DB_HOST,
+      host     : process.env.DB_HOST,
+      // host     : process.env.DATABASE_URL || process.env.DB_HOST,
       user     : process.env.DB_USER,
       password : process.env.DB_PASS,
       database : process.env.DB_NAME || 'midterm',
